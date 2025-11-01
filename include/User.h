c75bdaabc1a51f080f3a86c56192ea83676b6188
purrrhaps <musahassan7971@gmail.com>
2025-11-02 00:25:06 +0500

    feat: design User ADT

--- include/User.h
+++ include/User.h
@@ -1,4 +1,27 @@
 #ifndef USER_H
 #define USER_H
+#include <string>
+#include <list>
 
+class User {
+private:
+    std::string userName;
+    std::string email;
+    std::string password;
+    int age;
+    char gender;
+    std::string location;
+    bool isPrivate;
+    std::list<User*> friends;
+    std::string userId;
+public:
+    User();
+    void createPost();
+    void changePrivacySetting();
+    void sendRequest();
+    void declineRequest();
+    void viewOwnProfile();
+    void viewOtherProfile(User* other);
+    void viewFeed();
+};
 #endif //USER_H
