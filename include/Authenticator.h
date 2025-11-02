c25c6b318863ecf6fa7dbd8654b77e062cf7c71f
purrrhaps <musahassan7971@gmail.com>
2025-11-02 17:18:26 +0500

    feat: design Authenticator class

---
+++ include/Authenticator.h
@@ -0,0 +1,16 @@
+#ifndef AUTHENTICATOR_H
+#define AUTHENTICATOR_H
+#include <string>
+#include <vector>
+class User;
+class FakeBook;
+
+class Authenticator{
+private:
+    std::string fileName;
+public:
+    Authenticator(std::string _fileName);
+    User* login(std::vector<User*>& userList);
+    User* signUp(std::vector<User*>& userList);
+};
+#endif //AUTHENTICATOR_H
